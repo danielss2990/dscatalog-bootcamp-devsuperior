fe7303eba959ab93c4a14ae21fd4986e672c246f
Daniel Silva de Souza <danielss2990@gmail.com>
2020-09-30 16:42:26 -0300

    POST Category

--- backend/dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -36,10 +36,20 @@ public class CategoryService {
 	
 	}
 
-	@Transactional
+	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
 		Category entity = obj.orElseThrow(()-> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity= repository.save(entity);
+		
 		return new CategoryDTO(entity);
 	}
 
