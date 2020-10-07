cdb1fa871cdd01be81d0dba982366bd49e68c6e9
Daniel Silva de Souza <danielss2990@gmail.com>
2020-10-07 14:23:49 -0300

    Finish home page

---
+++ front-web/src/core/components/Buttonicon/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.scss';
+import { ReactComponent as ArrowIcon } from '../../assets/Images/arrow.svg';
+
+
+type Props = {
+    text: string;
+}
+
+const Buttonicon = ({ text }: Props) => (
+    <div className="d-flex">
+        <button className="btn btn-primary btn-icon">
+            <h5>{text}</h5>
+        </button>
+        <div className="btn-icon-content">
+            <ArrowIcon />
+        </div>
+    </div>
+);
+export default Buttonicon;
