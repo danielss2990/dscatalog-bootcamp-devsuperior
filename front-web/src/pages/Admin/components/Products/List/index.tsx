f0b909351bcaa5bd78fcf4eeb3e59a8567ed59e1
Daniel Silva de Souza <danielss2990@gmail.com>
2020-11-19 15:48:10 -0300

    Integrate product listing with backend

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,17 +1,51 @@
-import React from 'react';
-import {useHistory} from 'react-router-dom';
+import Pagination from 'core/Pagination';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Card from '../Card';
 
 const List = () => {
+
+    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
     const history = useHistory();
-    
+
+    console.log(productsResponse);
+
+    useEffect(() => {
+        const params = {
+            page: activePage,
+            linesPerPage: 4
+        }
+        //iniciar o loader
+        setIsLoading(true);
+        makeRequest({ url: '/products', params })
+            .then(response => setProductsResponse(response.data))
+            .finally(() => {
+                //finalizar o loader
+                setIsLoading(false);
+            });
+    }, [activePage]);
+
     const handleCreate = () => {
-        history.push('/admin/products/create');    
+        history.push('/admin/products/create');
     }
     return (
         <div className="admin-products-list">
             <button className="btn btn-primary btn-lg" onClick={handleCreate}>
                 ADICIONAR
             </button>
+            <div className="admin-list-container">
+                {productsResponse?.content.map(product => (
+                    <Card product={product} key={product.id} />
+                ))}
+                {productsResponse &&
+                    (<Pagination totalPages={productsResponse.totalPages}
+                        activePage={activePage}
+                        onChange={page => setActivePage(page)} />)}
+            </div>
         </div>
     );
 }
