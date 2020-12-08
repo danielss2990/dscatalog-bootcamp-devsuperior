d3c40ae18fe028dc120f384e182861a4fa998c3c
Daniel Silva de Souza <danielss2990@gmail.com>
2020-12-08 15:12:00 -0300

    Correcting other files

--- front-web/src/core/utils/request.ts
+++ front-web/src/core/utils/request.ts
@@ -1,7 +1,6 @@
 import axios, { Method } from 'axios';
 import { CLIENT_ID, CLIENT_SECRET, getSessionData, logout } from './auth';
 import qs from 'qs';
-import history from './history';
 
 type RequestParams = {
     method?: Method;
